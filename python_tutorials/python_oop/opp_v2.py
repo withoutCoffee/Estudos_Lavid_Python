3ce668cc4012d8db7484709434e55b7db7d8ab6f
withoutCoffee <jonasbdc1998@gmail.com>
2020-03-11 10:29:17 -0300

    add codes python oop 2

--- python_tutorials/python_oop/opp_v2.py
+++ python_tutorials/python_oop/opp_v2.py
@@ -13,7 +13,9 @@ class Employee:
         self.last = last
         self.pay = pay
         self.email = first +'.'+ last + '@company.com'
-    
+        
+    # Decora a função com decorator
+    @property
     def fullname(self):
         """[Este método concatena o primeiro com o
         ultimo nome do funcionário e retorna uma string]
@@ -23,10 +25,15 @@ class Employee:
         """        
         return f'{self.first} {self.last}'
     
+    @property
     def apply_raise(self):
         """[Este método aplica um aumento no salário de 4%]
         """        
         self.pay  = self.pay * self.raise_amount
+    
+        # Métodos mágicos, onde com o mesmos podemos fazer sobrecaga de operadores
+        # como por exemplo, +,-,*,/, respestivamento para os método:
+        # __add__, __sub__, __mul__, __div__
 
 # Herança da classe Employee, a classe Developer herda os atributos e métodos da classe Employee 
 class Developer(Employee):
@@ -52,15 +59,24 @@ class Manager(Employee):
             self.employees = employees
         
     def add_emp(self, emp):
+        """[Adiciona um employee]
+        
+        Arguments:
+            emp {[employee]} -- [employee]
+        """        
         self.employees.append(emp)
         
     def remove_emp(self,emp):
+        """[Remove um employee]
+        """
         if emp in self.employees:
             self.employees.remove(emp)
             
     def print_emps(self):
+        """[Printa nome completo de todo os employees do manager]
+        """        
         for cont,emp in enumerate(self.employees):
-            print(f"[{cont}] {emp.fullname()}")
+            print(f"[{cont}] {emp.fullname}")
         
         
 ep = Employee('Jonas','Oliveira',123123)
@@ -72,11 +88,11 @@ print(dev.prog_lang)
 dev_2 = Developer('Test', 'Employee', 60000, 'Java')
 
 mgr_1 = Manager('Sue', 'Smith', 90000, [dev])
-
-
 print(mgr_1.email)
 
+# Adiciona e remove dev2 
 mgr_1.add_emp(dev_2)
 mgr_1.remove_emp(dev_2)
 
+# Printa todos os devs pertecentos ao manager 1
 mgr_1.print_emps()
